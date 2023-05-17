2a66f69b7b4c5e4440650310378b15962697a7a9
Ashe-png <ashish.khatakho11@gmail.com>
2023-05-17 18:05:49 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/views/login_view.dart';
+import 'package:my_app/views/register_view.dart';
+import 'package:my_app/views/verify_email_view.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +16,48 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            // if (user?.emailVerified ?? false) {
+            //   // return const Text('Done');
+            // } else {
+            //   return const VerifyEmailView();
+            // }
+            // return const Text('Done');
+            return const Text('Done');
+          default:
+            return const Text('Loading...');
+        }
+      },
+    );
+  }
 }
